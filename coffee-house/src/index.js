acfddd6c990a875985652ee3b977de16fbba31d1
yury.matusevich <vfnectdbx78@gmail.com>
2025-10-02 01:01:43 +0400

    feat: add spa routing

--- coffee-house/src/index.js
+++ coffee-house/src/index.js
@@ -1,1 +1,14 @@
 import './style.css';
+
+//==========================ROUTING===========================//
+
+async function renderPage() {
+  //TODO Load html template
+}
+
+document.querySelectorAll('a[data-link]').forEach((link) => {
+  link.addEventListener('click', async (e) => {
+    e.preventDefault();
+    await renderPage();
+  })
+})
